3dedea380c2b9dd6db4224c9ffd088584bd6ffa3
danienl1 <le.daniel.nam@gmail.com>
2017-11-13 21:59:07 -0800

    hh

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ function facebook()
 {
 	window.location = "https://www.facebook.com/daniel.ton.714";
 }
+
+function linkedin()
+{
+	window.location = "https://www.linkedin.com/in/daniel-le-154b41135/";
+}
